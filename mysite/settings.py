531f39510cf70534633a8e18e211daf0dee754a0
Soham Dasgupta <s.dasgupta@gmail.com>
2021-08-18 22:12:03 +0530

    changed css

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--nw09i&3-5s^26c4^i8@3p9%m8ukr67yfis!07vt@i-%qy_=0m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
